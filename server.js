43dfda13cc41bccfe632215261f151521c9503ad
bronolaronjie <103414335+bronolaronjie@users.noreply.github.com>
2025-09-25 00:42:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get('/', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Proxy running on port ${PORT}`);
+
 });
